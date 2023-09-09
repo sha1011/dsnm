a6752627e8d66b17f1e4e8b98a240c12cf05f870
sha1011 <sha1011@users.noreply.github.com>
2023-09-09 01:31:50 +0000

    deploy: 623bd66ec751806b0241008a8f360c8f8e400c09

--- _nuxt/static/1694221021/charity-donation/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1694221021",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcharity-donation",config:{name:"Dommeti Suryanarayana Murthy",dates:"November 17th 1933 - September 2nd 2023",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
